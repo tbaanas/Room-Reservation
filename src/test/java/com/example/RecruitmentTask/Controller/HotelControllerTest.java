48c0e67a41dede2c58b0c70136fa7d7fc911d512
Tomasz Banaś <52421749+tomaszban98@users.noreply.github.com>
2022-08-06 14:06:10 +0200

    Update Hotel-reservation Test

--- src/test/java/com/example/RecruitmentTask/Controller/HotelControllerTest.java
+++ src/test/java/com/example/RecruitmentTask/Controller/HotelControllerTest.java
@@ -1,41 +1,31 @@
 package com.example.RecruitmentTask.Controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class HotelControllerTest {
 
 
 
     @Test
-    void testtest() throws IOException {
-        URL url = new URL("localhost:8888/seta");
+    void testGetHotelReservation() throws IOException {
+
+        URL url = new URL("http://localhost:8888/hotel-reservations?id=1");
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Accept", "application/json");
+        con.setRequestMethod("GET");
         con.setDoOutput(true);
-        String jsonInputString = "{\"start\":\"TEXT\"}";
-
-        try(OutputStream os = con.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
-            os.write(input, 0, input.length);
-        }
-
 
         try(BufferedReader br = new BufferedReader(
                 new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
             StringBuilder response = new StringBuilder();
+            Assert.notNull(br);
             String responseLine = null;
             while ((responseLine = br.readLine()) != null) {
                 response.append(responseLine.trim());
